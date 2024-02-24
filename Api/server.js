424ab6b1cc3b84225b0b2adf26b90cd0457fa1f2
Sumit Patil <jigneshpatil1723@gmail.com>
2024-02-24 10:48:19 +0530

    create post api route

--- Api/server.js
+++ Api/server.js
@@ -1,17 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoute from "./routes/user.routes.js"
-import authRoute from "./routes/auth.routes.js"
+import userRoute from "./routes/user.routes.js";
+import authRoute from "./routes/auth.routes.js";
+import postRoute from "./routes/post.routes.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
-mongoose.connect(
- process.env.MongoURL
-)
-.then( ()=>{
+mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("Mongo Connected  ")
-})
-.catch((err)=>{
+}).catch((err)=>{
 console.log(err);
 });
 const app = express();
@@ -24,6 +21,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/post', postRoute)
 
 app.use((err,req,res,next) =>{
   const statusCode = err.statusCode || 500;
